23770fcf143c5418bbc75e9aac7ff876d425b8b4
ramee <rameesky@yahoo.co.jp>
2023-11-23 11:51:50 +0900

    update

--- 04-sample/src/main/java/com/sample/main/GrpcClientConfiguration.java
+++ 04-sample/src/main/java/com/sample/main/GrpcClientConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(prefix = "app", name = "role", havingValue = "grpc-client")
+//@ConditionalOnProperty(prefix = "app", name = "role", havingValue = "grpc-client")
 @ComponentScan({"com.sample.grpc.client"})
 public class GrpcClientConfiguration {
 
